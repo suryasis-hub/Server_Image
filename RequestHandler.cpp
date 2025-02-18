737dc5931956cfe4a97ceca7d70af06d2e7de2a0
suryasis-hub <suryasishirokpauldev@gmail.com>
2025-02-18 12:52:15 +0530

    Update RequestHandler.cpp: server request changes

--- RequestHandler.cpp
+++ RequestHandler.cpp
@@ -1,48 +1,24 @@
-#include <pistache/http.h>
-#include <pistache/router.h>
-#include <pistache/endpoint.h>
+#include "RequestHandler.h"
 #include <iostream>
-#include <unordered_map>
-#include <functional>
 
-using namespace Pistache;
-
-
-std::string handleHello(const Http::Request& request) 
-{
+std::string handleHello(const Pistache::Http::Request& request) {
     return "Hello from Pistache!";
 }
 
-std::string handleStatus(const Http::Request& request) 
-{
+std::string handleStatus(const Pistache::Http::Request& request) {
     return "Server is running...";
 }
 
-class RequestHandler 
-{
-    private:
-        std::unordered_map<std::string, std::function<std::string(const Http::Request&)>> routerMapForFunction;
+// Corrected function definition
+void RequestHandler::onRequest(const Pistache::Http::Request& request, Pistache::Http::ResponseWriter response) {
+    std::string path = request.resource();
 
-    public:
-        RequestHandler() 
-        {
-            // Register handler functions
-            routerMapForFunction["/hello"] = handleHello;
-            routerMapForFunction["/status"] = handleStatus;
-        }
-
-        void onRequest(const Http::Request& request, Http::ResponseWriter response) 
-        {
-            std::string path = request.resource();
-            auto it = routerMapForFunction.find(path);
-            if (it != routerMapForFunction.end()) 
-            {
-                std::string responseData = it->second(request);  // Call the mapped function
-                response.send(Http::Code::Ok, responseData);
-            } 
-            else 
-            {
-                response.send(Http::Code::Not_Found, "404 - Not Found");
-            }
-        }
-};
+    // Check if the path exists in the router map
+    auto it = routerMapForFunction.find(path);
+    if (it != routerMapForFunction.end()) {
+        std::string responseData = it->second(request);  // Call the mapped function
+        response.send(Pistache::Http::Code::Ok, responseData);
+    } else {
+        response.send(Pistache::Http::Code::Not_Found, "404 - Not Found");
+    }
+}
