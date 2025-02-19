fcd6f2db75353150f84a60868e8c2bcc05f3ecf8
suryasis-hub <suryasishirokpauldev@gmail.com>
2025-02-19 11:17:45 +0530

    Update pistacheServer.cpp

--- pistacheServer.cpp
+++ pistacheServer.cpp
@@ -9,7 +9,7 @@ int main()
 
 {
     Http::Endpoint server(Address(Ipv4::any(), Port(9081)));
-    auto opts = Http::Endpoint::options().threads(1);
+    auto opts = Http::Endpoint::options().threads(1).maxRequestSize(1024 * 1024 * 10);
     server.init(opts);
     auto handler = std::make_shared<RequestHandler>();
     server.setHandler(handler);
